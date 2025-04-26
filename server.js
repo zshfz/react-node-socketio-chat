9fb48a7bfc89070c12f0b06d6f209eb639c6b31d
zshfz <zshfz8634@gmail.com>
2025-04-26 17:39:00 +0900

    fix: 라우트 경로 수정

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ io.on("connection", (client) => {
 // 정적 파일 서빙
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (_, res) => {
+app.get("/*", (_, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
